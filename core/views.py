39c8510d4bc2af00d7f9f3cad190d16ecb014189
kaelzubs <donmart4u@gmail.com>
2025-10-29 14:31:23 +0000

    Initial one

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from catalog.models import Product
 from django.db.models import Q
 from django.http import HttpResponse
